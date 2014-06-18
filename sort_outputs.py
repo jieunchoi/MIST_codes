b47fde83e70260474bbda772cb109e76e3c37058
Chris <jchoi37@ucsc.edu>
2014-06-18 12:05:14 -0700

    mass formatting test 5

--- sort_outputs.py
+++ sort_outputs.py
@@ -51,26 +51,26 @@ def gen_summary(rawdirname):
         if (len(errcontent) > 1):
             if '=>> PBS: job killed: walltime' in errcontent[1]:
                 status = 'FAILED'
-                reason = '"job killed, hit a walltime limit"'
+                reason = "'job killed, hit a walltime limit'"
             else:
                 status = 'FAILED'
-                reason = '"Unknown error, please check"'
+                reason = "'Unknown error, please check'"
         else:
             for line in outcontent[-30:]:
                 if 'termination code' in line:
-                    termination_reason = line.split('termination code: ')[1].split('\n')[0]
+                    termination_reason = "'"+line.split('termination code: ')[1].split('\n')[0]+"'"
                     break
                 if 'failed in do_relax_num_steps' in line:
-                    termination_reason = '"failed during preMS"'
+                    termination_reason = "'failed during preMS'"
                     break
             for line in outcontent[-50:]:  
                 if (' stopping because of convergence problems' in line) or \
                            ('terminated evolution: convergence problems' in line):
                     status = 'FAILED'
-                    reason = '"'+termination_reason+'"'
+                    reason = "'"+termination_reason+"'"
                 if (line == outcontent[-1]) & (status == ''):
                     status = 'OK'
-                    reason = '"'+termination_reason+'"'
+                    reason = "'"+termination_reason+"'"
         
         #get the runtime
         dates = subprocess.Popen('grep [0-9][0-9]:[0-9][0-9]:[0-9][0-9] ' + listoutfiles[index], shell=True, stdout=subprocess.PIPE)
@@ -83,7 +83,7 @@ def gen_summary(rawdirname):
             runtime = str(datetime.timedelta(seconds=datetime.timedelta.total_seconds(end-start)))
         #if it only returns starttime
         except:
-            runtime = '"exceeded walltime"'
+            runtime = "'exceeded walltime'"
             
         #populate the stat_summary dictionary
         stat_summary[mass] = "{:<15}".format(status) + "{:<50}".format(reason) + "{:<20}".format(runtime)
@@ -99,7 +99,7 @@ def gen_summary(rawdirname):
     f.writerow(['','',''])
     
     for key in keys:
-        f.writerow(["{:<15}".format(key), stat_summary[key]])
+        f.writerow(["{:<15}".format(key)s, stat_summary[key]])
         
 def sort_histfiles(rawdirname):
 
