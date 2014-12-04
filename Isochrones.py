054da9bcf61a7e7d6f42c80fac02f311b3124e39
Chris <jchoi37@ucsc.edu>
2014-12-03 22:16:49 -0500

    np bugs

--- Isochrones.py
+++ Isochrones.py
@@ -30,9 +30,9 @@ class MESA_Isochrones:
         first_line=fread('(a25,i5)')
         age_line=fread('(a6,e20.10,a5)')
         eep_line=fread('(a25,2i5)')
-        def num_line(cols): return fread('(1x,i4,{:d}i32)'.format(int(cols-1)))
-        def col_line(cols): return fread('(2x,a3,{:d}a32)'.format(int(cols-1)))
-        def data_line(cols): return fread('(i5,{:d}e32.16)'.format(int(cols-1)))
+        def num_line(cols): return fread('(1x,i4,{:d}i32)'.format(np.int(cols-1)))
+        def col_line(cols): return fread('(2x,a3,{:d}a32)'.format(np.int(cols-1)))
+        def data_line(cols): return fread('(i5,{:d}e32.16)'.format(np.int(cols-1)))
     
         #read the first two lines
         junk,num_ages = first_line.read(f.readline())
@@ -47,15 +47,15 @@ class MESA_Isochrones:
             #remove extra spaces in names
             names = tuple([name.strip() for name in names])
             #adjust column number for python
-            formats=tuple([int32]+[float64 for i in range(num_cols-1)])
+            formats=tuple([np.int32]+[np.float64 for i in range(num_cols-1)])
             iso=zeros((num_eeps),{'names':names,'formats':formats})
             #read through EEPs for each isochrone
             for eep in range(num_eeps):
                 x=f.readline().split()
                 y=[]
             #this business with x and y is inelegant
-                y.append(int(x[0]))
-                [y.append(z) for z in map(float64,x[1:])]
+                y.append(np.int(x[0]))
+                [y.append(z) for z in map(np.float64,x[1:])]
                 iso[eep]=tuple(y)
             if iage<num_ages-1:
                 f.readline()
@@ -88,8 +88,8 @@ class MESA_Isochrones:
         small=1.0e-5
         min_log_age-=small
         max_log_age+=small
-        if int(fig_num) >= 0:
-            plt.figure(num=int(fig_num),figsize=(12,10))
+        if np.int(fig_num) >= 0:
+            plt.figure(num=np.int(fig_num),figsize=(12,10))
         else:
             plt.figure(num=None,figsize=(12,10))
         plt.xlabel('log(Teff) [K]', fontsize=20)
@@ -111,8 +111,8 @@ class MESA_Isochrones:
         min_log_age-=small
         max_log_age+=small
         fs=18
-        if int(fig_num) >= 0:
-            plt.figure(num=int(fig_num),figsize=(8,8))
+        if np.int(fig_num) >= 0:
+            plt.figure(num=np.int(fig_num),figsize=(8,8))
         else:
             plt.figure(num=None,figsize=(8,8))
         mag_list = self.BCTable.names
@@ -135,8 +135,8 @@ class MESA_Isochrones:
         min_log_age-=small
         max_log_age+=small
         fs=20
-        if int(fig_num) >= 0:
-            plt.figure(num=int(fig_num),figsize=(8,8))
+        if np.int(fig_num) >= 0:
+            plt.figure(num=np.int(fig_num),figsize=(8,8))
         else:
             plt.figure(num=None,figsize=(8,8))
         plt.xlabel(r'$\log(T_c)$',fontsize=fs)
