6e5f1f7bef6055f367c8e441069d777d8a91fe93
Jieun Choi <jieunchoi@cfa.harvard.edu>
2016-02-17 19:08:07 -0500

    only plot non-interpolated tracks

--- mesa_plot_grid.py
+++ mesa_plot_grid.py
@@ -32,7 +32,7 @@ def plot_HRD(gridname, logg=False):
     #Assumes gridname has the form MIST_vXX/feh_XXX_afe_XXX
     lowest_dir = gridname.split('/')[-1]
     grid_dir = os.path.join(mistgrid_dir, gridname)
-    filelist = glob.glob(os.path.join(grid_dir, 'eeps/*M.track.eep*'))
+    filelist = glob.glob(os.path.join(grid_dir, 'eeps/*M.track.eep'))
     for file in filelist:
         starmass = float(file.split('eeps/')[1].split('M')[0])/100.0
         star = mp.EEPfile(file)
