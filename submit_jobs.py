1f0ccc5d4ef97087ee237edc97a938f58433df39
jieun choi <jieun.choi@cfa.harvard.edu>
2017-02-23 16:35:47 -0800

    bug fix

--- submit_jobs.py
+++ submit_jobs.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
         afe_fmt = 'afe+'+str(aFe)
         
     #Run Aaron's code to get the abundances
-    os.system("./"+os.path.join(os.environ["XA_CALC_DIR"], "initial_xa_calculator/initial_xa_calculator") +\
+    os.system("./"+os.path.join(os.environ["XA_CALC_DIR"], "initial_xa_calculator") +\
         " " +  net_name + " " + str(FeH) + " " +str(aFe))
         
     #Zbase needs to be set in MESA for Type II opacity tables. Get this from a file produced by Aaron's code
